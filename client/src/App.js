8e6e40033c47a9e1ec19bb3a4839c7161fa8a1d4
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-03-07 10:53:52 +0000

    Created client

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import {useEffect, useState} from 'react';
+import './App.css';
+
+function App() {
+
+  const[message, setMessage] =useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000')
+    .then((res) => res.json())
+    .then((data) => setMessage(data.message));
+
+  }, []);
+
+  return (
+    <>
+    <h1>Intro to Express</h1>
+    <p>The server said: {message}</p>
+    </>
+  );
+}
+
+export default App;
